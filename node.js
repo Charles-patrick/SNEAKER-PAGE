e03bdb756b6d1ebb019efa60bbb2a18ed7231e62
Charles-patrick <ebukacharles006@gmail.com>
2023-02-22 21:40:14 +0100

    third commit

--- node.js
+++ node.js
@@ -3,7 +3,6 @@ const shoeOne = document.querySelector(".shoe1")
 const shoeTwo = document.querySelector(".shoe2")
 const shoeThree = document.querySelector(".shoe3")
 const shoeFour = document.querySelector(".shoe4")
-const numBer = document.querySelector("#zero")
 const minusBtn = document.querySelector(".minus-btn")
 const addBtn = document.querySelector(".add-btn")
 const dropDown = document.querySelector(".dropdown")
@@ -12,26 +11,32 @@ const bodyEl = document.getElementsByName("body")
 const addtocartBtn = document.querySelector(".add-to-cart")
 const emptyCartel = document.querySelector(".empty-cart")
 const fallLimitedinfo = document.querySelector(".empty-cart-hidden")
-console.log(emptyCartel)
-console.log(fallLimitedinfo)
-
+let prIce = document.querySelector(".price")
+let numberOfshoes = document.querySelector(".no-of-shoes")
+let totalEl = document.querySelector(".total-el")
+const numBer = document.querySelector("#zero")
+let moneyEl = document.querySelector(".money").innerHTML
+let moneyvalueEl = document.querySelector(".money-value").innerHTML
 let count = 0
 numBer.textContent = count
+prIce.textContent = "$" + moneyvalueEl
 
-// dropDown.forEach ((x) => {
-//   window.onclick = function (event) {
-//     if(event.target == x){
-//         dropdown.style.display = "none"
-//     }
-// }
-// });
+
+
+
+
+
+
+
 
 fallLimitedinfo.classList.add("hidden")
 
 addtocartBtn.addEventListener("click", ()=>{
     emptyCartel.classList.add("hidden")
     fallLimitedinfo.classList.remove("hidden")
+    numberOfshoes.textContent = numBer.textContent
   })
+
 
 
 
@@ -54,7 +59,13 @@ shoeOne.addEventListener("click", ()=>{
 
 shoeFour.addEventListener("click", ()=>{
      bigCont.innerHTML = "<img src='image-product-4.jpg' class='img-one'>"
-  })     
+  })    
+  
+  function timesFxn() {
+   let multiPlication = moneyvalueEl * numBer.textContent   
+       totalEl.textContent = "$" + multiPlication
+  }
+  
 
  minusBtn.addEventListener("click", ()=>{
     
@@ -63,10 +74,12 @@ shoeFour.addEventListener("click", ()=>{
      } else {
         numBer.textContent = count
      }
-    })     
-
+      timesFxn()
+    }) 
+    
  addBtn.addEventListener("click", ()=>{
      numBer.textContent = count += 1
   })    
+
 
 
